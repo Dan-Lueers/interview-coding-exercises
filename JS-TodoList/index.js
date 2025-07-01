c5d92b78f1fd490e1ea27c1627f82572cbbef91b
Dan-Lueers <dan.lueers@gmail.com>
2025-07-01 16:37:21 +0200

    implemented styles and add and delete functionality

--- JS-TodoList/index.js
+++ JS-TodoList/index.js
@@ -1,1 +1,46 @@
 console.log("dkasl;dkal;s");
+
+
+function onAddClick() {
+  // get value from input
+  const input = document.getElementById("input-item");
+  const value = input.value;
+  addItemToList(value);
+  // clear input
+  input.value = "";
+}
+
+function onDeleteClick(event) {
+  // get the parent list item of the button
+  const button = event.target;
+  const li = button.parentElement.parentElement;
+  // remove the list item
+  li.remove();
+}
+
+function addItemToList(value) {
+  // create new list item
+  const li = document.createElement("li");
+  const span = document.createElement("span");
+  span.textContent = value;
+
+  // create checkbox
+  const checkbox = document.createElement("input");
+  checkbox.type = "checkbox";
+
+  // create delete button
+  const button = document.createElement("button");
+  button.textContent = "delete";
+  button.onclick = function () {
+    li.remove();
+  };
+
+  // append elements to list item
+  span.appendChild(checkbox);
+  span.appendChild(button);
+  li.appendChild(span);
+
+  // append list item to the list
+  const ul = document.querySelector("ul");
+  ul.appendChild(li);
+}
