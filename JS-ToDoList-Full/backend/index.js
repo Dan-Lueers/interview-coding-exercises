52348bd6f622e2788a8cc4aaf4aeaa339deb4adb
Dan-Lueers <dan.lueers@gmail.com>
2025-07-02 11:52:57 +0200

    FULL-TODO: Setup basic express server

---
+++ JS-ToDoList-Full/backend/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const tasks = [];
+
+app.get("/", (req, res) => {
+  res.send("aaaa");
+});
+
+app.post("/tasks", (req, res) => {
+  let task = req.body.task;
+  if (!task) {
+    return res.status(400).send("Task is required");
+  }
+  tasks.push(task);
+  res.status(201).send({ message: "Task added successfully", task });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
